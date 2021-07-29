e77f8fe5b5f282fd8b517574f0f1ce9e59e15cbe
Lee-Rhaan <rhaanking@gmail.com>
2021-07-29 12:28:37 +0200

    Initial commit

---
+++ src/main/java/com/example/redditclone/model/Vote.java
@@ -0,0 +1,30 @@
+package com.example.redditclone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
